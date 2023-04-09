6f81dc5d519eab1e1b91142a3dbdf60b8dc2c35d
Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2023-04-09 23:53:33 +0600

    Class number 1 was about How to make a calculator How to use function and about return keyword

---
+++ class1.py
@@ -0,0 +1,20 @@
+def myFunction(num1:float, oparation:str, num2:float):
+    if(oparation == '/'):
+        result = num1 / num2
+    elif(oparation == '*'):
+        result = num1 * num2
+    elif(oparation == '+'):
+        result = num1 + num2
+    elif(oparation == '-'):
+        result = num1 - num2
+    elif(oparation == '**'):
+        result == num1 ** num2
+    elif(oparation == '%'):
+        result = num1 % num2
+    elif(oparation == 'root'):
+        result = num1 ** (1 / num2)
+    return result
+
+while True:
+    x = myFunction(float(input()), input(), float(input()))
+    print(x)
